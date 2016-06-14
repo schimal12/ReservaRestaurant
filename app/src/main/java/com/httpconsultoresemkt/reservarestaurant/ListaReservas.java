e879e43cc9136b7e200ed4c78fb05f658ce6a793
schimal12@gmail.com <schimal12@gmail.com>
2016-06-13 22:43:54 -0500

    ListView
    La lista de restaurantes se cambio a una ListView

--- app/src/main/java/com/httpconsultoresemkt/reservarestaurant/ListaReservas.java
+++ app/src/main/java/com/httpconsultoresemkt/reservarestaurant/ListaReservas.java
@@ -1,20 +1,25 @@
 package com.httpconsultoresemkt.reservarestaurant;
 
+import android.app.ListActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.httpconsultoresemkt.reservarestaurant.Models.Reservacion;
 import com.httpconsultoresemkt.reservarestaurant.Services.RegisterSQLHelper;
 
 import java.util.ArrayList;
 
-public class ListaReservas extends AppCompatActivity {
+public class ListaReservas extends ListActivity {
 
-    ListView lv;
     ArrayList<Reservacion> modelList;
     CustomAdapterReservas adapter;
 
@@ -23,14 +28,14 @@ public class ListaReservas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_reservas);
-
         //crear el listview de restaurantes
-        lv = (ListView) findViewById(R.id.listView);
         modelList = new ArrayList<Reservacion>();
         adapter = new CustomAdapterReservas(getApplicationContext(), modelList, getApplicationContext(), this);
-        lv.setAdapter(adapter);
+        setListAdapter(adapter);
+        setContentView(R.layout.activity_lista_reservas);
         setDataListView();
     }
+
 
     private void setDataListView() {
 
@@ -64,5 +69,35 @@ public class ListaReservas extends AppCompatActivity {
             }while (c.moveToNext());
         }
         adapter.notifyDataSetChanged();
+    }
+
+    public void onListItemClick(ListView l, View v, final int position, long id) {
+        new AlertDialog.Builder(this)
+                .setTitle("Borrar reserva")
+                .setMessage("¿Estás seguro que quieres borrar esta reserva?")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        RegisterSQLHelper registerSQLHelper = new RegisterSQLHelper(ListaReservas.this,"DBReservacion",null,1);
+                        SQLiteDatabase db = registerSQLHelper.getWritableDatabase();
+
+                        String nombre = modelList.get(position).getNombre().toString();
+
+                        if(db != null){
+                            db.execSQL("DELETE FROM Reservacion WHERE name="+"'"+nombre+"'");
+                            db.close();
+                            Toast.makeText(ListaReservas.this,"Reservación Borrada",Toast.LENGTH_SHORT).show();
+                            modelList.clear();
+                            setDataListView();
+                        }
+
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 }
